a27d3a83d129963956fd8fe6d667daf6ef64fbd2
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2023-08-22 20:24:57 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>‘I am Peter Akasi Mba, this is my first React application.’ </p>
+    </>
+  );
+}
+
+export default App
