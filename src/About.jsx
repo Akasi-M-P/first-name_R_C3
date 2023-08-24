f691152d0c4a24c0dbb33a04d4653d3b93f14cd6
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2023-08-24 22:26:46 +0000

    simple user portfolio page

---
+++ src/About.jsx
@@ -0,0 +1,73 @@
+const About = () => {
+  return (
+    <>
+      <h2>Peter M. Akasi</h2>
+      <h4>Educational background</h4>
+      <ul>
+        <li>
+          <h5>
+            University of Professional Studies, Accra Bachelor of Science - BS,
+            Information Technology Management.
+          </h5>
+          <h6>Aug 2022 - Aug 2024</h6>
+        </li>
+        <li>
+          <h5>
+            University of Professional Studies, Accra Diploma , Information
+            Technology Management.
+          </h5>
+          <h6>Aug 2020 - Aug 2022</h6>
+        </li>
+        <li>
+          <h4>Professional background</h4>
+          <p>
+            Software Engineer with more than 1 year of experience in utilizing
+            JavaScript, React.JS, Node.JS, Next.JS, Express.JS, MySQL, MongoDB
+            and REST API in crafting dynamic and responsive interfaces, along
+            with developing server-side applications and databases. Committed to
+            delivering innovative business solutions for a diverse clientele. A
+            valuable contributor throughout the Software Development Lifecycle,
+            Project Management, and I.T Sourcing and Procurement processes.
+          </p>
+        </li>
+        <li>
+          <h4>Skills</h4>
+        </li>
+        <li>
+          <h5>Python (Programming Language) </h5>
+        </li>
+        <li>
+          <h5>Database Administration</h5>
+        </li>
+        <li>
+          <h5> IT Project Implementation</h5>
+        </li>
+        <li>
+          <h5>C++</h5>
+        </li>
+        <li>
+          <h5>Visual Basic .NET (VB.NET) </h5>
+        </li>
+        <li>
+          <h5>T Procurement</h5>
+        </li>
+        <li>
+          <h5>Python (Programming Language) </h5>
+        </li>
+        <li>
+          <h5>Systems Analysis </h5>
+        </li>
+        <li>
+          <h5> HTML5</h5>
+        </li>
+        <li>
+          <h5>CSS3</h5>
+        </li>
+        <li>
+          <h5> JavaScript ES6</h5>
+        </li>
+      </ul>
+    </>
+  );
+};
+export default About;
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import About from "./About";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Hello World</h1>
-      <p>‘I am Peter Akasi Mba, this is my first React application.’ </p>
+      <About />
     </>
   );
 }
 
-export default App
+export default App;
